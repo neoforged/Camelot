4102d3012521775b4d960f25cafb3f25ffc1503d
Matyrobbrt <matyrobbrt@gmail.com>
2023-08-23 15:05:51 +0300

    Allow embeds to have fields

--- src/main/java/net/neoforged/camelot/script/ScriptMap.java
+++ src/main/java/net/neoforged/camelot/script/ScriptMap.java
@@ -5,6 +5,10 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.graalvm.polyglot.Value;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * A wrapper for a {@link Value} that is treated like a map.
  *
@@ -38,6 +42,39 @@ public record ScriptMap(Value value) {
     }
 
     /**
+     * Gets the value with the given {@code key} as a boolean.
+     *
+     * @param key the key of the value to query
+     * @return the value with the given key, or {@code null} if one doesn't exist
+     */
+    @Nullable
+    public Boolean getBoolean(String key) {
+        final Value val = value.getMember(key);
+        if (val == null) return null;
+        return val.asBoolean();
+    }
+
+    /**
+     * Gets the value with the given {@code key} as a boolean.
+     * @param key the key of the value to query
+     * @return the value with the given key as a list, or an {@link List#of() empty list} if one doesn't exist
+     */
+    public List<Value> getList(String key) {
+        final Value val = value.getMember(key);
+        if (val == null) return List.of();
+        if (val.hasIterator()) {
+            final List<Value> values = new ArrayList<>();
+            final var itr = val.getIterator();
+            while (itr.hasIteratorNextElement()) {
+                values.add(itr.getIteratorNextElement());
+            }
+            return values;
+        } else {
+            return List.of(val);
+        }
+    }
+
+    /**
      * Converts this map into an embed.
      */
     public MessageEmbed asEmbed() {
@@ -53,6 +90,12 @@ public record ScriptMap(Value value) {
                 builder.setTitle(scr.getString("value"), scr.getString("url"));
             }
         }
+
+        getList("fields").stream().map(ScriptMap::new).forEach(field -> builder.addField(
+                Objects.requireNonNull(field.getString("name")),
+                Objects.requireNonNull(field.getString("value")),
+                Objects.requireNonNullElse(field.getBoolean("inline"), false)
+        ));
 
         final Integer colour = getInt("color");
         if (colour != null) {
