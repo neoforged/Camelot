891e4ff66986a9f32e716a20fe04019174a8adb9
Matyrobbrt <matyrobbrt@gmail.com>
2023-09-17 22:52:55 +0300

    Allow thumbnails in embeds

--- src/main/java/net/neoforged/camelot/script/ScriptMap.java
+++ src/main/java/net/neoforged/camelot/script/ScriptMap.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * A wrapper for a {@link Value} that is treated like a map.
@@ -56,6 +57,7 @@ public record ScriptMap(Value value) {
 
     /**
      * Gets the value with the given {@code key} as a boolean.
+     *
      * @param key the key of the value to query
      * @return the value with the given key as a list, or an {@link List#of() empty list} if one doesn't exist
      */
@@ -81,15 +83,7 @@ public record ScriptMap(Value value) {
         final EmbedBuilder builder = new EmbedBuilder();
         builder.setDescription(getString("description"));
 
-        final Value title = value.getMember("title");
-        if (title != null) {
-            if (title.isString()) {
-                builder.setTitle(ScriptUtils.toString(title));
-            } else {
-                final ScriptMap scr = new ScriptMap(title);
-                builder.setTitle(scr.getString("value"), scr.getString("url"));
-            }
-        }
+        stringOrObject("title", builder::setTitle, obj -> builder.setTitle(obj.getString("value"), obj.getString("url")));
 
         getList("fields").stream().map(ScriptMap::new).forEach(field -> builder.addField(
                 Objects.requireNonNull(field.getString("name")),
@@ -102,6 +96,32 @@ public record ScriptMap(Value value) {
             builder.setColor(colour);
         }
 
+        builder.setThumbnail(getString("thumbnail"));
+        builder.setImage(getString("image"));
+
+        stringOrObject("author", builder::setAuthor, obj -> builder.setAuthor(
+                obj.getString("name"), obj.getString("url"), obj.getString("iconUrl")
+        ));
+
         return builder.build();
+    }
+
+    /**
+     * Consumes the member with the given {@code key} as a string or object.
+     *
+     * @param key      the key of the member to get
+     * @param ifString the consumer to call if the member is a string
+     * @param ifObject the consumer to call if the member is a map
+     */
+    public void stringOrObject(String key, Consumer<String> ifString, Consumer<ScriptMap> ifObject) {
+        final Value val = this.value.getMember(key);
+        if (val != null) {
+            if (val.isString()) {
+                ifString.accept(ScriptUtils.toString(val));
+            } else {
+                final ScriptMap scr = new ScriptMap(val);
+                ifObject.accept(scr);
+            }
+        }
     }
 }
