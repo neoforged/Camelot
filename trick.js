fdbdb93a5a866c21ca03112bfb186258edbdf992
Matyrobbrt <matyrobbrt@gmail.com>
2024-01-05 14:10:37 +0200

    [skip ci] That shouldn't have been committed...

--- trick.js
+++
@@ -1,6 +0,0 @@
-const description = 'Sleep'
-const [target] = options.optPositional(0, {description: 'Who should sleep', required: true}).parse()
-
-function execute() {
-    reply(`${target} go to sleep already`)
-}
