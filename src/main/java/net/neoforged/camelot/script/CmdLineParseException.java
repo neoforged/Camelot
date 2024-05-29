d71bb6372017c97ee97118fbb56f5bf414f5526d
Matyrobbrt <matyrobbrt@gmail.com>
2024-05-29 13:44:57 +0300

    Publish all artifacts

--- src/main/java/net/neoforged/camelot/script/CmdLineParseException.java
+++ src/main/java/net/neoforged/camelot/script/CmdLineParseException.java
@@ -1,8 +1,10 @@
 package net.neoforged.camelot.script;
 
+import java.util.List;
+
 /**
  * A specific exception thrown by {@link ScriptOptions#parse()} when parsing the arguments failed. <br>
- * This allows {@link ScriptUtils#execute(ScriptContext, String, String)} to detect when an exception is argument
+ * This allows {@link ScriptUtils#execute(ScriptContext, String, List)} to detect when an exception is argument
  * parsing failure (starts with the prefix {@value #PREFIX}) and report it as such (without a stacktrace).
  */
 public final class CmdLineParseException extends Exception {
